c4fc1371f5627c69695008e57989cb64521cc11e
zzx79189917 <zzx79189917@gmail.com>
2016-01-16 19:23:01 +0800

    Initial Commit

---
+++ ADishTests/ADishTests.swift
@@ -0,0 +1,36 @@
+//
+//  ADishTests.swift
+//  ADishTests
+//
+//  Created by 邹圳巡 on 16/1/16.
+//  Copyright © 2016年 zouzhenxun. All rights reserved.
+//
+
+import XCTest
+@testable import ADish
+
+class ADishTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
